9e32168678bd880911e458a19ef68e78b6ed0b8d
8carlos8 <charlifloresromero04@gmail.com>
2024-03-05 19:07:41 -0600

    agregación de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarteleraController;
+use App\Http\Controllers\BoletoController;
+use App\Http\Controllers\EstacionamientoController;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\OrganizadorController;
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\SalaController;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +26,25 @@ Route::get('/', function () {
 });
 
 Route::resource('Carteleras', CarteleraController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', BoletoController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', EstacionamientoController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', EventoController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', OrganizadorController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', PersonaController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', SalaController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('Carteleras', UsuarioController::class)
     ->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
