b9fd614661d35e1aef42ccf151fa7e842f5ba34a
8carlos8 <charlifloresromero04@gmail.com>
2024-04-04 18:00:12 -0600

    actualizacion de la base

--- DBcentro.sql
+++ DBcentro.sql
@@ -19,10 +19,14 @@
 -- Volcando datos para la tabla centro.cajons: ~0 rows (aproximadamente)
 
 -- Volcando datos para la tabla centro.carteleras: ~0 rows (aproximadamente)
+INSERT INTO `carteleras` (`id`, `id_evento`, `id_sala`, `estado`, `inicio`, `fin`, `created_at`, `updated_at`) VALUES
+	(1, 2, 1, 3, '2024-03-01 00:00:00', '2024-04-18 00:00:00', '2024-04-02 01:27:42', '2024-04-02 01:50:05');
 
 -- Volcando datos para la tabla centro.estacionamientos: ~0 rows (aproximadamente)
 
 -- Volcando datos para la tabla centro.eventos: ~0 rows (aproximadamente)
+INSERT INTO `eventos` (`id`, `id_organizador`, `nombre`, `tipo`, `duracion`, `foto`, `created_at`, `updated_at`) VALUES
+	(2, 3, 'FC Barcelona', 'd', '2024-04-03 00:00:00', _binary 0x6469616772616d612e6a7067, '2024-04-02 01:23:49', '2024-04-02 01:23:49');
 
 -- Volcando datos para la tabla centro.migrations: ~0 rows (aproximadamente)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -37,17 +41,27 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(9, '2024_03_06_004347_create_boletos_table', 1),
 	(10, '2024_03_17_192854_create_cajons_table', 1);
 
--- Volcando datos para la tabla centro.organizadors: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla centro.organizadors: ~2 rows (aproximadamente)
+INSERT INTO `organizadors` (`id`, `id_persona`, `estado`, `razonSoc`, `direccion`, `created_at`, `updated_at`) VALUES
+	(3, 5, 1, 'Hola', 'Gdasdas', '2024-04-01 02:04:01', '2024-04-01 02:05:10');
 
 -- Volcando datos para la tabla centro.personal_access_tokens: ~0 rows (aproximadamente)
 
--- Volcando datos para la tabla centro.personas: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla centro.personas: ~3 rows (aproximadamente)
 INSERT INTO `personas` (`id`, `nombre`, `a_paterno`, `a_materno`, `fecha_nac`, `telefono`, `created_at`, `updated_at`) VALUES
-	(1, 'carlos', 'romero', 'flores', '2024-03-18 11:55:51', '2481726753', NULL, NULL);
+	(1, 'Carlos', 'romero', 'flores', '2024-03-31 00:00:00', '2481726753', NULL, '2024-04-01 02:01:19'),
+	(5, 'Sara', 'dasdasd', 'Vicente', '2024-03-26 00:00:00', '2481650226', '2024-03-27 04:57:19', '2024-03-27 04:57:19'),
+	(6, 'Pedro', 'Vicente', 'Vicente', '2024-04-10 00:00:00', '2481650226', '2024-04-02 03:50:33', '2024-04-02 03:50:33'),
+	(7, 'Yo', 'a', 'a', '2024-04-04 00:00:00', '1234567890', '2024-04-05 05:11:56', '2024-04-05 05:11:56');
 
 -- Volcando datos para la tabla centro.salas: ~0 rows (aproximadamente)
+INSERT INTO `salas` (`id`, `estado`, `nombre`, `asientos`, `created_at`, `updated_at`) VALUES
+	(1, 1, 'Hsdads', 100, '2024-04-01 02:26:49', '2024-04-01 02:33:05');
 
--- Volcando datos para la tabla centro.usuarios: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla centro.usuarios: ~2 rows (aproximadamente)
+INSERT INTO `usuarios` (`id`, `id_persona`, `estado`, `correo`, `username`, `password`, `rol`, `created_at`, `updated_at`) VALUES
+	(1, 6, 1, '1@gmail.com', 'Pepito', '123', 1, '2024-04-02 03:50:33', '2024-04-02 03:50:33'),
+	(2, 7, 1, '2.@gmail.com', 'ola', '$2y$12$Xc29QbU6y8R7XU1X5h0rxeRscl93GbHQEFTYkPIEDuNC6/7pm1536', 1, '2024-04-05 05:11:57', '2024-04-05 05:11:57');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
