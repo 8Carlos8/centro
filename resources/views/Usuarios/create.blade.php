6625b36cf9ce5f97510049af46145031c7acc08d
Diego <diegoramireztome@gmail.com>
2024-04-07 19:19:52 -0600

    Estilos

--- resources/views/Usuarios/create.blade.php
+++ resources/views/Usuarios/create.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport">
-    <link rel="icon" type="image/x-icon" href="../imagenes/CULTURA1.png">
+    <link rel="icon" type="image/x-icon" href="{{ asset ('imagenes/CULTURA1.png')}}">
     <title>CC Siglo XXI - Registrarse</title>
-    <link rel="stylesheet" href="../css/custom.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
